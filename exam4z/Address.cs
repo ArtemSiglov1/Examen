d45e761a17a18e6a86d05c58b1b34f2961215f47
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-03 13:27:28 +0300

    Add files via upload

---
+++ exam4z/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam4z
+{
+    class Address
+    {
+        string index, country, city, street;
+        int house,apartment;
+
+        public string Index { get => index; set => index = value; }
+        public string Country { get => country; set => country = value; }
+        public string City { get => city; set => city = value; }
+        public string Street { get => street; set => street = value; }
+        public int House { get => house; set => house = value; }
+        public int Apartment { get => apartment; set => apartment = value; }
+        public Address(string index,string country,string city,string street,int house,int apartment)
+        {
+            Index = index;
+            Country = country;
+            City = city;
+            Street = street;
+            House = house;
+            Apartment = apartment;
+        }
+        public override string ToString()
+        {
+            return $"{index} {country} {city} {street} {house} {apartment}";
+        }
+        
+    }
+}
---
+++ exam4z/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam4z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address("3300", "Moldova", "Tiraspol", "Krasnodonskaya", 36, 25);
+            Console.WriteLine(address.ToString());
+            Console.ReadLine();
+        }
+    }
+}
