479ef92e9b113c867013b395c7252decd55361ea
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-10 14:15:23 +0300

    Add files via upload

---
+++ Exam23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine("Каким цветом вывести текст");
+            foreach (Pat.Colors colors in Enum.GetValues(typeof(Pat.Colors)))
+            {
+                Console.WriteLine($"{(int)colors} - {colors}");
+            }
+            Console.WriteLine("Введите цифру цвета:");
+            int color = int.Parse(Console.ReadLine());
+            Pat.Print(str,color);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exam37/CalcS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam37
+{
+    /// <summary>
+    /// 1 объявление делегата
+    /// </summary>
+    /// <param name="a">сторона а</param>
+    /// <param name="b">сторона b</param>
+    /// <param name="c">сторона с</param>
+    /// <returns>площадь</returns>
+    public delegate double Message(double a,double b,double c);
+    static class CalcS
+    {
+       public static double Area(double a, double b, double c)
+        {
+            if (a < b + c && b < a + c && c < a + b)
+            {
+                double p = (a + b + c) / 2;
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            else { return 0; }
+        }
+       
+    }
+}
