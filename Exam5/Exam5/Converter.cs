f0d2574f75045c67b12ee7f13f67433b32f74ac7
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-18 20:28:21 +0300

    Add files via upload

---
+++ Exam5/Exam5/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam5
+{
+    internal class Converter
+    {
+        double usd, eur, rub;
+        public Converter(double usd,double eur,double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+      
+        public double Usd { get => usd; set => usd = value; }
+        public double Eur { get => eur; set => eur = value; }
+        public double Rub { get => rub; set => rub = value; }
+        public override string ToString()
+        {
+            return $"Usd-{usd}\nEur-{eur}\nRub-{rub}";
+        }
+
+
+    }
+}
---
+++ Exam5/Exam5/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(0.311, 0.285, 28.29);
+            Converter converter1 = new Converter(2.627, 3.487, 0.035);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1-Из BYN\n2-В BYN\n0-Exit");
+                int choise = int.Parse(Console.ReadLine());
+                switch (choise)
+                {
+                    case 1:
+                        Console.WriteLine($"Текущий курс-{converter.ToString()}");
+                        while (true)
+                        {
+                            double count = 0 ;
+                            Console.WriteLine("1-Usd,2-Eur,3-Rub,0-Exit");
+                            int choise1 = int.Parse(Console.ReadLine());
+                            if (choise1 != 0)
+                            {
+                                Console.WriteLine("Введите сумму в BYN:");
+                                 count= double.Parse(Console.ReadLine());
+                            }
+                            switch (choise1)
+                            {
+                                case 1:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter.Usd}"); break;
+                                case 2:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter.Eur}"); break;
+                                case 3:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter.Rub}"); break;
+                                case 0: return;
+                                default:
+                                    Console.WriteLine("again"); break;
+                            }
+                        }
+                    case 2:
+                        Console.WriteLine($"Текущий курс-{converter1.ToString()}");
+                        while (true)
+                        {
+                            double count = 0;
+                            Console.WriteLine("1-Usd,2-Eur,3-Rub,0-Exit");
+                            int choise1 = int.Parse(Console.ReadLine());
+                            if (choise1 != 0)
+                            {
+                                Console.WriteLine("Введите сумму в BYN:");
+                                count = double.Parse(Console.ReadLine());
+                            }
+                            switch (choise1)
+                            {
+                                case 1:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter1.Usd}"); break;
+                                case 2:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter1.Eur}"); break;
+                                case 3:
+                                    Console.WriteLine($"Сумма в долларах:{count * converter1.Rub}"); break;
+                                case 0: return;
+                                default:
+                                    Console.WriteLine("again"); break;
+                            }
+                        }
+                       
+                    case 0:flag = false;break;
+                    default:
+                        Console.WriteLine("Again");break;
+                }
+            }
+        }
+
+    }
+}
