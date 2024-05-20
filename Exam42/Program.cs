d770c4fecc3aa20c3d16e3c379f66be0f5e0b8ca
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-20 09:07:20 +0300

    Add files via upload

---
+++ Exam42/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle<int> triangle = new Triangle<int>(4,5,6);
+            Triangle<uint> triangle1 = new Triangle<uint>(6, 7, 8);
+            Triangle<double> triangle2 = new Triangle<double>(6.1,7.1,8.1);
+            Console.WriteLine($"{triangle.ToString()}\n{triangle1.ToString()}\n{triangle2.ToString()}");
+            Console.WriteLine($"{triangle.Sqare()},{triangle.TriangleTrue()}\n{triangle1.Sqare()},{triangle1.TriangleTrue()}\n{triangle2.Sqare()},{triangle2.TriangleTrue()}");
+            Console.ReadLine();       
+        }
+    }
+}
---
+++ Exam42/Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam42
+{
+    class Triangle<ttype>
+    {
+        ttype a, b, c;
+
+        public ttype A { get => a; set =>  a=value<(dynamic)0?(dynamic)0:value; }
+        public ttype B { get => b; set => b = value < (dynamic)0 ? (dynamic)0 : value; }
+        public ttype C { get => c; set => c = value < (dynamic)0 ? (dynamic)0 : value; }
+        public Triangle() { }
+        public Triangle(ttype a, ttype b, ttype c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public override string ToString()
+        {
+            return $"A {a},B {b},C {c}";
+        }
+        public double Sqare()
+        {
+            double p=((dynamic)a+b+c)/2;
+            return Math.Sqrt(p * ((dynamic)p - a) * ((dynamic)p - b) * ((dynamic)p - c));
+        }
+        public string TriangleTrue()
+        {
+            if (a < (dynamic)b + c && b < (dynamic)a + c && c < (dynamic)a + b)
+            {return "Треугольник существует";
+            }
+            else
+            {
+                return"Треугольник не существует";
+            }
+        }
+    }
+}
