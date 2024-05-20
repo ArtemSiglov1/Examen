3c37372ad36535a76a569ae75afb1c12e5ab2571
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-20 09:16:57 +0300

    Update Program.cs

--- Exam42/Program.cs
+++ Exam42/Program.cs
@@ -11,7 +11,7 @@ namespace Exam42
         static void Main(string[] args)
         {
             Triangle<int> triangle = new Triangle<int>(4,5,6);
-            Triangle<uint> triangle1 = new Triangle<uint>(6, 7, 8);
+            Triangle<uint> triangle1 = new Triangle<uint>(6u, 7u, 8u);
             Triangle<double> triangle2 = new Triangle<double>(6.1,7.1,8.1);
             Console.WriteLine($"{triangle.ToString()}\n{triangle1.ToString()}\n{triangle2.ToString()}");
             Console.WriteLine($"{triangle.Sqare()},{triangle.TriangleTrue()}\n{triangle1.Sqare()},{triangle1.TriangleTrue()}\n{triangle2.Sqare()},{triangle2.TriangleTrue()}");
