a5dd305e2970378eda83b58e1da423907871e4b2
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-20 08:42:03 +0300

    Add files via upload

---
+++ Exam41/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam41
+{
+    class Point<ttype>
+    {
+        ttype x, y;
+        public Point() { }
+        public ttype X { get => x; set => x = value; }
+        public ttype Y { get => y; set => y = value; }
+        public Point(ttype x,ttype y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return $"{x};{y}";
+        }
+    }
+}
---
+++ Exam41/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> point = new Point<int>(2,2);
+            Point<uint> point1 = new Point<uint>(5,5);
+            Point<double> point2 = new Point<double>(7.1, 7.1);
+            Console.WriteLine($"A({point})\nB({point1})\nC({point2})");
+            Console.ReadLine();
+        }
+    }
+}
