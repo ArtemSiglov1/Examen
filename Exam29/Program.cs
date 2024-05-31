88fc8a022a0258ea22bea70663fd22cefbe21613
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-31 14:23:12 +0300

    Add files via upload

---
+++ Exam29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Raschet = new List<int>();
+           Tovar tovar= Tovar.Init();
+            Raschet.Add(Tovar.Raschet(tovar));
+            Tovar tovar1 = Tovar.Init();
+            Raschet.Add(Tovar.Raschet(tovar1));
+            int z = 0;
+            foreach(var item in Raschet)
+            {
+                z+= item;
+            }
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam29/Tovar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam29
+{
+    class Tovar
+    {
+        string name;
+        int price;
+        int count;
+
+        public Tovar(string name, int price, int count)
+        {
+            this.name = name;
+            this.price = price;
+            this.count = count;
+        }
+
+        public string Name { get; set; }
+        public int Price { get => price; set => price = value <= 1 && value >= 20 ? 0 : value; }
+        public int Count { get => count; set => count = value <= 0 && value >= 10 ? 0 : value; }
+        public static int Raschet(Tovar tovar)
+        {
+            return tovar.price * tovar.count;
+        }
+        
+        public static Tovar Init()
+        {
+            string name=Console.ReadLine();
+            int price=int.Parse(Console.ReadLine());
+            int count=int.Parse(Console.ReadLine());
+            return new Tovar(name, price, count);
+        }
+    }
+}
