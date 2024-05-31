ca9bb5c8662e8b242256e14fbed3d104bea6f25f
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-05-31 13:58:58 +0300

    Add files via upload

---
+++ Exam28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tour tour = new Tour();
+           
+            Tour tour1 = new Tour("Gawai");
+
+            Tour tour2 = new Tour("Efiopia", 10);
+            tour.TourCalc();
+
+            tour1.TourCalc("Gawaiii");
+
+            tour2.TourCalc("Efiopiaiii", 10);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exam28/Tour.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam28
+{
+    class Tour
+    {
+        string name;
+        int price;
+        int countDays;
+
+        public Tour() { }
+        public Tour(string name)
+        {
+            this.name = name;
+        }
+        public Tour(string name,int countDays):this(name)
+        {
+            this.countDays = countDays;
+        }
+
+        public void TourCalc()
+        {
+            name = "Минское море";
+            price = 0;
+            Console.WriteLine($"{name} {price}");
+        }
+        public void TourCalc(string name)
+        {
+            this.name = name;
+            price = 50;
+            countDays = 1;
+            Console.WriteLine($"{this.name} {price} {countDays}");
+        }
+        public void TourCalc(string name,int countDays)
+        {
+            this.name = name;
+            price = 50*countDays;
+           this.countDays =countDays;
+            Console.WriteLine($"{this.name} {price} {this.countDays}");
+        }
+       
+    }
+}
